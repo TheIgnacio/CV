d6f895565645fe199137a730c61cdb4b0edbcb41
Eric <nacho100898@gmail.com>
2023-01-20 15:35:31 -0300

    CV 1.v

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+function ocultar (){
+    document.getElementById('personal').style.display = 'none';
+}
+
+function mostrar (){
+    document.getElementById('personal').style.display = 'block';
+}
+
+function ocultarEM (){
+    document.getElementById('empleo').style.display = 'none';
+}
+
+function mostrarEM (){
+    document.getElementById('empleo').style.display = 'block';
+}
+
+function mostrarCPRE (){
+    document.getElementById('CPRE').style.display = 'block';
+}
+
+
+
